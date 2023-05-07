5b9170b9e62b4006d9cd8084991a6e9b6516fd06
hm-hermus <eduard.hermus@hm.edu>
2023-05-07 22:10:59 +0200

    improved runtime by ca. 10 Seconds

---
+++ cmake-build-debug/CMakeFiles/EC2023.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EC2023.dir/main.cpp.obj"
+  "CMakeFiles/EC2023.dir/main.cpp.obj.d"
+  "bin/EC2023.exe"
+  "bin/EC2023.exe.manifest"
+  "bin/EC2023.pdb"
+  "lib/libEC2023.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EC2023.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
